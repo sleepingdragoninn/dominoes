ddd0042117cfc749efe9669b13e57547a87e91e4
sisby-folk <sleepingdragoninn@gmail.com>
2025-08-19 16:54:05 +1000

    hardcode voxel shapes for version support

--- src/main/java/dev/sisby/dominoes/DominoBlock.java
+++ src/main/java/dev/sisby/dominoes/DominoBlock.java
@@ -23,54 +23,26 @@ import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.DirectionTransformation;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import net.minecraft.world.block.WireOrientation;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static dev.sisby.dominoes.DominoShapes.*;
+import static dev.sisby.dominoes.DominoShapes.VOXEL_NORTH_SOUTH_STACK_FORWARDS;
+import static dev.sisby.dominoes.DominoShapes.VOXEL_SOUTH_WEST_EAST_BACKWARDS;
+import static dev.sisby.dominoes.DominoShapes.VOXEL_SOUTH_WEST_EAST_FORWARDS;
 
 public class DominoBlock extends Block implements Falling {
-	protected static final VoxelShape VOXEL_NS = VoxelShapes.cuboid(0.25, 0, 0.4375, 0.75, 0.875, 0.5625);
-	protected static final VoxelShape VOXEL_NS_FORWARDS = VoxelShapes.cuboid(0.25, 0, 0.125, 0.75, 0.125, 1);
-	protected static final VoxelShape VOXEL_NS_BACKWARDS = VoxelShapes.transform(VOXEL_NS_FORWARDS, DirectionTransformation.ROT_180_FACE_XZ);
-	protected static final VoxelShape VOXEL_EW = VoxelShapes.transform(VOXEL_NS, DirectionTransformation.SWAP_XZ);
-	protected static final VoxelShape VOXEL_EW_BACKWARDS = VoxelShapes.transform(VOXEL_NS_FORWARDS, DirectionTransformation.SWAP_XZ);
-	protected static final VoxelShape VOXEL_EW_FORWARDS = VoxelShapes.transform(VOXEL_NS_BACKWARDS, DirectionTransformation.SWAP_XZ);
-	protected static final VoxelShape VOXEL_NE = VoxelShapes.union(
-		VoxelShapes.cuboid(8.5 / 16.0, 0, 6 / 16.0, 10.5 / 16.0, 14 / 16.0, 8 / 16.0),
-		VoxelShapes.cuboid(9.5 / 16.0, 0, 5 / 16.0, 11.5 / 16.0, 14 / 16.0, 7 / 16.0),
-		VoxelShapes.cuboid(10.5 / 16.0, 0, 4 / 16.0, 12.5 / 16.0, 14 / 16.0, 6 / 16.0),
-		VoxelShapes.cuboid(11.5 / 16.0, 0, 3 / 16.0, 13.5 / 16.0, 14 / 16.0, 5 / 16.0),
-		VoxelShapes.cuboid(12.5 / 16.0, 0, 2 / 16.0, 14.5 / 16.0, 14 / 16.0, 4 / 16.0)
-	);
-	protected static final VoxelShape VOXEL_NS_STACK = VoxelShapes.union(
-		VoxelShapes.cuboid(0.25, 0, 0.125, 0.75, 0.875, 0.25),
-		VoxelShapes.cuboid(0.25, 0, 0.75, 0.75, 0.875, 0.875)
-	);
-	protected static final VoxelShape VOXEL_EW_STACK = VoxelShapes.transform(VOXEL_NS_STACK, DirectionTransformation.SWAP_XZ);
-	protected static final VoxelShape VOXEL_ES = VoxelShapes.transform(VOXEL_NE, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_SW = VoxelShapes.transform(VOXEL_ES, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_WN = VoxelShapes.transform(VOXEL_SW, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_NE_COLLAPSED = VoxelShapes.cuboid(0.5, 0, 0, 1, 0.125, 0.875);
-	protected static final VoxelShape VOXEL_ES_COLLAPSED = VoxelShapes.transform(VOXEL_NE_COLLAPSED, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_SW_COLLAPSED = VoxelShapes.transform(VOXEL_ES_COLLAPSED, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_WN_COLLAPSED = VoxelShapes.transform(VOXEL_SW_COLLAPSED, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_NEW = VoxelShapes.union(VOXEL_NE, VoxelShapes.transform(VOXEL_NE, DirectionTransformation.INVERT_X));
-	protected static final VoxelShape VOXEL_ESN = VoxelShapes.transform(VOXEL_NEW, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_SWE = VoxelShapes.transform(VOXEL_ESN, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_WNS = VoxelShapes.transform(VOXEL_SWE, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_NEW_COLLAPSED = VoxelShapes.union(VOXEL_NE_COLLAPSED, VoxelShapes.transform(VOXEL_NE_COLLAPSED, DirectionTransformation.INVERT_X));
-	protected static final VoxelShape VOXEL_ESN_COLLAPSED = VoxelShapes.transform(VOXEL_NEW_COLLAPSED, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_SWE_COLLAPSED = VoxelShapes.transform(VOXEL_ESN_COLLAPSED, DirectionTransformation.ROT_90_Y_NEG);
-	protected static final VoxelShape VOXEL_WNS_COLLAPSED = VoxelShapes.transform(VOXEL_SWE_COLLAPSED, DirectionTransformation.ROT_90_Y_NEG);
-
 	public static final EnumProperty<Collapsed> COLLAPSED = EnumProperty.of("collapsed", Collapsed.class);
 	public static final EnumProperty<Shape> SHAPE = EnumProperty.of("shape", Shape.class);
 	public static final BooleanProperty COLLAPSING = BooleanProperty.of("collapsing");
@@ -81,6 +53,23 @@ public class DominoBlock extends Block implements Falling {
 	public DominoBlock(Settings settings) {
 		super(settings);
 		this.setDefaultState(this.stateManager.getDefaultState().with(COLLAPSED, Collapsed.NONE).with(SHAPE, Shape.NORTH_SOUTH).with(COLLAPSING, false));
+		Dominoes.LOGGER.info("\n{}", Arrays.stream(Shape.values()).map(shape -> {
+			final StringBuilder s = new StringBuilder();
+			List<String> TYPES = List.of("STANDING", "FORWARDS", "BACKWARDS");
+			List<Function<Shape, VoxelShape>> GETTERS = List.of(Shape::getShapeStanding, Shape::getShapeForwards, Shape::getShapeBackwards);
+			for (int i = 0; i < 3; i++) {
+				s.append("\tpublic static final VoxelShape VOXEL_");
+				s.append(shape.name().toUpperCase());
+				s.append("_");
+				s.append(TYPES.get(i));
+				s.append(" = VoxelShapes.union(\n");
+				GETTERS.get(i).apply(shape).forEachBox((minX, minY, minZ, maxX, maxY, maxZ) -> s.append("\t\tVoxelShapes.cuboid(%s, %s, %s, %s, %s, %s),\n".formatted(minX, minY, minZ, maxX, maxY, maxZ)));
+				s.deleteCharAt(s.length() - 1);
+				s.deleteCharAt(s.length() - 1);
+				s.append("\n\t);\n");
+			}
+			return s.toString();
+		}).collect(Collectors.joining("")));
 	}
 
 	@Override
@@ -256,21 +245,21 @@ public class DominoBlock extends Block implements Falling {
 	public enum Shape implements StringIdentifiable {
 		// falling "forwards" means falling from first direction towards second direction
 		// straights
-		NORTH_SOUTH("north_south", List.of(Direction.NORTH, Direction.SOUTH), VOXEL_NS, VOXEL_NS_FORWARDS, VOXEL_NS_BACKWARDS),
-		EAST_WEST("east_west", List.of(Direction.EAST, Direction.WEST), VOXEL_EW, VOXEL_EW_FORWARDS, VOXEL_EW_BACKWARDS),
+		NORTH_SOUTH("north_south", List.of(Direction.NORTH, Direction.SOUTH), VOXEL_NORTH_SOUTH_STANDING, VOXEL_NORTH_SOUTH_FORWARDS, VOXEL_NORTH_SOUTH_BACKWARDS),
+		EAST_WEST("east_west", List.of(Direction.EAST, Direction.WEST), VOXEL_EAST_WEST_STANDING, VOXEL_EAST_WEST_FORWARDS, VOXEL_EAST_WEST_BACKWARDS),
 		// straight stacks
-		NORTH_SOUTH_STACK("north_south_stack", List.of(Direction.NORTH, Direction.SOUTH), VOXEL_NS_STACK, VOXEL_NS_FORWARDS, VOXEL_NS_BACKWARDS),
-		EAST_WEST_STACK("east_west_stack", List.of(Direction.EAST, Direction.WEST), VOXEL_EW_STACK, VOXEL_EW_FORWARDS, VOXEL_EW_BACKWARDS),
+		NORTH_SOUTH_STACK("north_south_stack", List.of(Direction.NORTH, Direction.SOUTH), VOXEL_NORTH_SOUTH_STACK_STANDING, VOXEL_NORTH_SOUTH_STACK_FORWARDS, VOXEL_NORTH_SOUTH_STACK_BACKWARDS),
+		EAST_WEST_STACK("east_west_stack", List.of(Direction.EAST, Direction.WEST), VOXEL_EAST_WEST_STACK_STANDING, VOXEL_EAST_WEST_STACK_FORWARDS, VOXEL_EAST_WEST_STACK_BACKWARDS),
 		// corners
-		NORTH_EAST("north_east", List.of(Direction.NORTH, Direction.EAST), VOXEL_NE, VOXEL_NE_COLLAPSED, VOXEL_NE_COLLAPSED),
-		EAST_SOUTH("east_south", List.of(Direction.EAST, Direction.SOUTH), VOXEL_ES, VOXEL_ES_COLLAPSED, VOXEL_ES_COLLAPSED),
-		SOUTH_WEST("south_west", List.of(Direction.SOUTH, Direction.WEST), VOXEL_SW, VOXEL_SW_COLLAPSED, VOXEL_SW_COLLAPSED),
-		WEST_NORTH("west_north", List.of(Direction.WEST, Direction.NORTH), VOXEL_WN, VOXEL_WN_COLLAPSED, VOXEL_WN_COLLAPSED),
+		NORTH_EAST("north_east", List.of(Direction.NORTH, Direction.EAST), VOXEL_NORTH_EAST_STANDING, VOXEL_NORTH_EAST_FORWARDS, VOXEL_NORTH_EAST_BACKWARDS),
+		EAST_SOUTH("east_south", List.of(Direction.EAST, Direction.SOUTH), VOXEL_EAST_SOUTH_STANDING, VOXEL_EAST_SOUTH_FORWARDS, VOXEL_EAST_SOUTH_BACKWARDS),
+		SOUTH_WEST("south_west", List.of(Direction.SOUTH, Direction.WEST), VOXEL_SOUTH_WEST_STANDING, VOXEL_SOUTH_WEST_FORWARDS, VOXEL_SOUTH_WEST_BACKWARDS),
+		WEST_NORTH("west_north", List.of(Direction.WEST, Direction.NORTH), VOXEL_WEST_NORTH_STANDING, VOXEL_WEST_NORTH_FORWARDS, VOXEL_WEST_NORTH_BACKWARDS),
 		// doubles
-		NORTH_EAST_WEST("north_east_west", List.of(Direction.NORTH, Direction.EAST, Direction.WEST), VOXEL_NEW, VOXEL_NEW_COLLAPSED, VOXEL_NEW_COLLAPSED),
-		EAST_SOUTH_NORTH("east_south_north", List.of(Direction.EAST, Direction.SOUTH, Direction.NORTH), VOXEL_ESN, VOXEL_ESN_COLLAPSED, VOXEL_ESN_COLLAPSED),
-		SOUTH_WEST_EAST("south_west_east", List.of(Direction.SOUTH, Direction.WEST, Direction.EAST), VOXEL_SWE, VOXEL_SWE_COLLAPSED, VOXEL_SWE_COLLAPSED),
-		WEST_NORTH_SOUTH("west_north_south", List.of(Direction.WEST, Direction.NORTH, Direction.SOUTH), VOXEL_WNS, VOXEL_WNS_COLLAPSED, VOXEL_WNS_COLLAPSED);
+		NORTH_EAST_WEST("north_east_west", List.of(Direction.NORTH, Direction.EAST, Direction.WEST), VOXEL_NORTH_EAST_WEST_STANDING, VOXEL_NORTH_EAST_WEST_FORWARDS, VOXEL_NORTH_EAST_WEST_BACKWARDS),
+		EAST_SOUTH_NORTH("east_south_north", List.of(Direction.EAST, Direction.SOUTH, Direction.NORTH), VOXEL_EAST_SOUTH_NORTH_STANDING, VOXEL_EAST_SOUTH_NORTH_FORWARDS, VOXEL_EAST_SOUTH_NORTH_BACKWARDS),
+		SOUTH_WEST_EAST("south_west_east", List.of(Direction.SOUTH, Direction.WEST, Direction.EAST), VOXEL_SOUTH_WEST_EAST_STANDING, VOXEL_SOUTH_WEST_EAST_FORWARDS, VOXEL_SOUTH_WEST_EAST_BACKWARDS),
+		WEST_NORTH_SOUTH("west_north_south", List.of(Direction.WEST, Direction.NORTH, Direction.SOUTH), VOXEL_WEST_NORTH_SOUTH_STANDING, VOXEL_WEST_NORTH_SOUTH_FORWARDS, VOXEL_WEST_NORTH_SOUTH_BACKWARDS);
 
 		public static final List<Shape> STRAIGHTS = List.of(NORTH_SOUTH, EAST_WEST);
 		public static final List<Shape> STACKS = List.of(NORTH_SOUTH_STACK, EAST_WEST_STACK);
